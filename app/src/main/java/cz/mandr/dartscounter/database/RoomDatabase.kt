2d01af24c7ed86e7dd951c2b719c396bbedeb1f1
mandr <andresikmichal@gmail.com>
2024-04-29 16:41:10 +0200

    Database
    Database 'games' and Dao interface with queries have been created in RoomDatabase.kt.

--- app/src/main/java/cz/mandr/dartscounter/database/RoomDatabase.kt
+++ app/src/main/java/cz/mandr/dartscounter/database/RoomDatabase.kt
@@ -1,4 +1,55 @@
 package cz.mandr.dartscounter.database
 
-class RoomDatabase {
+import android.content.Context
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface GameDao{
+
+    @Insert
+    fun insertGame(game: GameDTO)
+
+    @Query("DELETE FROM games WHERE gameID = :gameId")
+    fun deleteGameById(gameId: Int)
+
+    @Query("DELETE FROM games")
+    fun deleteAllGames()
+
+    @Query("SELECT * FROM games WHERE gameID = :gameId")
+    fun getGameById(gameId: Int) : Flow<GameDTO>
+
+    @Query("SELECT * FROM games")
+    fun getAllGames() : Flow<List<GameDTO>>
+
+    @Query("SELECT MAX(gameID) FROM games")
+    fun getMaxId() : Flow<Int>
+
+    @Query("SELECT COUNT(*) FROM games")
+    fun getRowsCount() : Flow<Int>
+
+}
+
+@Database(entities = [GameDTO::class], version = 1)
+abstract class MyRoomDatabase : RoomDatabase(){
+    abstract val gameDao: GameDao
+}
+
+private lateinit var INSTANCE: MyRoomDatabase
+
+fun getDatabase(context: Context) : MyRoomDatabase {
+    synchronized(MyRoomDatabase::class.java){
+        if(!::INSTANCE.isInitialized){
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                MyRoomDatabase::class.java,
+                "my_room_database").build()
+        }
+    }
+    return INSTANCE
 }
